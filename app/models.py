a2c17fc9283058dc1bec0bcb7deada56ca31fd52
landpack <landpack@sina.com>
2016-04-14 16:07:27 +0800

    Build Base Framework

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 from . import db
 from . import login_manager
 
@@ -76,7 +76,6 @@ class User(UserMixin, db.Model):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
 
-
     @property
     def password(self):
         raise AttributeError('password is not readable attribute')
@@ -136,6 +135,17 @@ class User(UserMixin, db.Model):
 
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+
+        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+                url=url, hash=hash, size=size, default=default, rating=rating
+        )
 
     def __repr__(self):
         return '<User %r>' % self.username
